1a065433d4d5d7003b744a0bff52998159485d34
Martin Stefcek <gcifko@gmail.com>
2025-11-07 20:42:28 +0100

    rename

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -2,7 +2,7 @@ use anyhow::Ok;
 
 use crate::echoes_of_enigmatus;
 
-mod year_2024_the_kingdom_of_algorithms;
+mod the_kingdom_of_algorithms_2024;
 
 pub fn run() -> anyhow::Result<()> {
     echoes_of_enigmatus::run()?;
